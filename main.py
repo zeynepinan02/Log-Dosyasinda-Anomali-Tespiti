a4815888df611777afb338e58f49fe0fa217c99d
zeynep inan <zeyzey6102@gmail.com>
2023-12-04 16:33:58 +0300

    dataset duzenlendi

--- main.py
+++ main.py
@@ -30,4 +30,32 @@ filter_value= ['forward','local','system','wireless','sdwan','app-ctrl','webfilt
 #belirledigimiz kolonun degerlerine gore filtreleme yapıyor. Kosulu saglayan satırları data frame'e kaydediyoruz.
 df_newColums=df_newColums[df_newColums[filter_column].isin(filter_value)]
 
+#json veri setinde dateTime alanı $date seklinde etiketli halde bulunuyor. Bunu dateTime tipine donusturur.
+df_newColums["datetime"] = pd.to_datetime(df_newColums["datetime"].apply(lambda x: x["$date"]))
+
+# veri setinde örnekleme yapıyor
+
+# Sınıflara göre gruplandırın
+logs = df_newColums.groupby('datetime')
+
+#birden fazla sütunla gruplama yapmak istersek
+#logs = df_newColums.groupby(['date', 'time'])
+
+ornekler = []
+
+# Her sınıftan eşit sayıda örnek alın
+for log_name, log in logs:
+    # Belirlediğiniz örnek sayısını ayarlayın (örneğin 50)
+    ornek_grup = log.sample(n=1, random_state=42)
+    ornekler.append(ornek_grup)
+
+# Stratified örnekleri birleştirin
+stratified_ornekler = pd.concat(ornekler)
+
+#datetime sutununu tekrar jsona uygun hale getiriyor
+stratified_ornekler["datetime"] = stratified_ornekler["datetime"].apply(lambda x: x.strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
+
+# Sonuçları yazdırın
+stratified_ornekler.to_json('DataSet.json', orient='records')
+
 
